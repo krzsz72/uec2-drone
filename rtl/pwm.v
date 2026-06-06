cfb596253790af73437b699552389e47bb11ebb4
krzsz72 <kpiziak@student.agh.edu.pl>
2026-06-06 19:03:20 +0200

    pwm resolution update now res up to 1us from prev 100us. Not tested

--- rtl/pwm.v
+++ rtl/pwm.v
@@ -5,13 +5,13 @@
 //      |______________________________________________|
 //      |                                              |
 //      |    Parameters and defaults                   |
-//      |        MAX_TICK  = 14'd9999                  |
-//      |        MAX_PWM   = 8'd199                    |
-//      |        eg. d_in = 50 ===== 50/200 fill       |
+//      |        MAX_TICK  = 7'd99                     |
+//      |        MAX_PWM   = 15'd19999                 |
+//      |        eg. d_in = 1000 ===== 10000/20000 fill|
 //      |                                              |
 //  ----| enable                                       |
-//  ==8=| d_in                                 PWM_out |----
-//      |                                          cnt |==8=
+// ==15=| d_in                                 PWM_out |----
+//      |                                          cnt |==15=
 //  ----| clk                                          |
 //      |______________________________________________|
 //
@@ -40,8 +40,7 @@
 //
 //  2) enable: Activates the PWM when logic high. PWM idles low when deactivated.
 //
-//  3) d_in: Is used to determine the duty cycle of the PWM. This input has a 
-//     bit width defined by the B parameter.
+//  3) d_in: Is used to determine the duty cycle of the PWM. currently 1us resolution
 //
 //** Signal Outputs ************************************************************
 //
@@ -52,32 +51,32 @@
 //
 
 module pwm #(
-   parameter MAX_TICK=14'd9999, //prescaler
-   parameter MAX_PWM=8'd199    //counter clamp for 100us pwm 50hz: 20ms / 100us = 200 steps
+   parameter MAX_TICK=7'd99, //prescaler
+   parameter MAX_PWM=15'd19999    //counter clamp for 1us pwm 50hz: 20ms / 1us = 20000 steps
 
 )
    (
     input wire clk, enable,
-    input wire [7:0] d_in,
+    input wire [14:0] d_in,
     output reg pwm,
-    output reg [7:0] cnt
+    output reg [14:0] cnt
    );
 
    
    // signal declaration
-   reg [7:0] D;   //data buffer reg
+   reg [14:0] D;   //data buffer reg
    reg tick_100us;
-   reg [13:0] prescale_cnt;
+   reg [6:0] prescale_cnt;
 
 
    always @(posedge clk) begin
       if(!enable)begin
          tick_100us<=1'b0;
-         prescale_cnt<=14'd0;
+         prescale_cnt<=7'd0;
       end else begin
          if(prescale_cnt>=MAX_TICK)begin
             tick_100us<=1'b1;
-            prescale_cnt<=14'd0;
+            prescale_cnt<=7'd0;
          end else begin
             tick_100us<=1'b0;
             prescale_cnt<=prescale_cnt+1'd1;
@@ -88,13 +87,13 @@ module pwm #(
    always @(posedge clk)begin
        if(!enable)begin
          pwm<=1'b0;
-         cnt<=8'd0;
-         D<=8'd0;
+         cnt<=15'd0;
+         D<=15'd0;
       end else begin 
          if(tick_100us)begin
             if(cnt>=MAX_PWM)begin
-               cnt<=8'd0;
-               if(d_in>8'd200) D<=8'd200;
+               cnt<=15'd0;
+               if(d_in>8'd20000) D<=15'd20000;
                else D<=d_in;
             end else begin
                cnt<=cnt+1'd1;
